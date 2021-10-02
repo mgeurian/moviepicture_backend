4763217714e9c04adec99e4d1beaba7a9b832c70
Matt Geurian <mgeurian@gmail.com>
2021-10-01 21:25:09 -0500

    swapped code from previous commit

---
+++ helpers/omdb.js
@@ -0,0 +1,22 @@
+const { omdbApiKey } = require('../config');
+const axios = require('axios');
+
+class OmdbWrapper {
+	static getBaseUrl() {
+		return `http://www.omdbapi.com/?apikey=${omdbApiKey}&type=movie`;
+	}
+
+	static async searchMoviesByTitle(title, page = 1) {
+		const url = `${this.getBaseUrl()}&s=${encodeURIComponent(title)}&page=${page}`;
+		const { data: movies } = await axios(url);
+		return movies;
+	}
+
+	static async getMovieByImdbId(id) {
+		const url = `${this.getBaseUrl()}&i=${id}`;
+		const { data: movie } = await axios(url);
+		return movie;
+	}
+}
+
+module.exports = OmdbWrapper;
---
+++ routes/movies.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/** Routes for movies. */
+
+const jsonschema = require('jsonschema');
+
+const express = require('express');
+const { ensureLoggedIn } = require('../middleware/auth');
+const movieSearchSchema = require('../schemas/movieSearch.json');
+const { BadRequestError, NotFoundError } = require('../expressError');
+const Movie = require('../models/movie');
+const OmdbWrapper = require('../helpers/omdb');
+
+const router = express.Router();
+
+/** GET /[searchTerm] => { movies }
+ *
+ * Returns { Poster, Title, Type, Year, imdbID }
+ *
+ *  **/
+router.get('/search', ensureLoggedIn, async (req, res, next) => {
+	try {
+		const validator = jsonschema.validate(req.query, movieSearchSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map((e) => e.stack);
+			throw new BadRequestError(errs);
+		}
+
+		const data = await OmdbWrapper.searchMoviesByTitle(req.query.q, req.query.page);
+		return res.json({ data });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+/** GET /[movie_id] => { movie }
+ *
+ * Returns { id, imdb_id, title, year, genre, plot, director, poster, imdb_rating }
+ *
+ *  **/
+
+router.get('/:id', ensureLoggedIn, async function(req, res, next) {
+	try {
+		const { id } = req.params;
+		const movie = await Movie.getById(id);
+
+		if (!movie) {
+			throw new NotFoundError(`Movie with id ${id} not found`);
+		}
+
+		return res.json({ data: movie });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+module.exports = router;
